0428860ec266baccd765e59ef723fb5a8d87968b
dgun <hdg20042@gmail.com>
2025-05-28 12:58:11 +0900

    commit message

--- myboard/settings.py
+++ myboard/settings.py
@@ -150,6 +150,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# settings.py
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # 올바른 설정
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
